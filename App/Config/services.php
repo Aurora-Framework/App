1a62ea24031ed308445374301e9670bf62d8aeee
VeeeneX <veeenex@gmail.com>
2015-07-04 10:54:49 +0200

    Creating ready to use skeleton

--- App/Config/services.php
+++ App/Config/services.php
@@ -1,12 +1,22 @@
 <?php
 $Injector = new Aurora\Injector();
 
-$Injector->define("Aurora\\Http\\Request",[
+$Injector->define("Aurora\\Http\\Request", [
 	":GET" => $_GET,
 	":POST" => $_POST,
 	":COOKIE" => $_COOKIE,
 	":FILES" => $_FILES,
 	":SERVER" => $_SERVER,
+]);
+
+$Loader = new Twig_Loader_Filesystem(APP."View/");
+$Twig = new Twig_Environment($loader, array(
+	'cache' => APP."Storage/Cache/",
+));
+$Twig->addExtension(new Aurora\Twig\Extension());
+
+$Injector->define("Aurora\\MVC\\Presenter", [
+	":Engine" => $Twig
 ]);
 
 return $Injector;
